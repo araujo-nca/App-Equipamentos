fd887eedd8928c7e013d9a47e8a418f70d7611e0
Michael Henrique <oliveirasantos.mh@gmail.com>
2019-10-22 20:10:54 -0300

    Declaração das váriaveis da interface - OK

--- Testando_TabelasCondutores.py
+++ Testando_TabelasCondutores.py
@@ -1,3 +1,4 @@
+from planilhas import Planilha
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtGui import QPixmap
@@ -13,25 +14,80 @@ class TabelaCondutores(QtWidgets.QMainWindow):
 
         self.show()
 
-        self.btm_configuracao_Unipolar = self.findChild(QtWidgets.QRadioButton, 'radioButton_Unipolar')
-        self.btm_configuracao_Tripolar = self.findChild(QtWidgets.QRadioButton, 'radioButton_Tripolar')
-        self.imagem_condutor = self.findChild(QtWidgets.QLabel, 'label_Imagem')
+
+        # Radio Buttons
+        # ----------------------------------------------------------------------------------------------
+
+        # Define o Radio Button para a configuração unipolar
+        self.radioButton_Unipolar = self.findChild(QtWidgets.QRadioButton, 'radioButton_Unipolar')
+
+        # Define o Radio Button para a configuração tripolar
+        self.radioButton_Tripolar = self.findChild(QtWidgets.QRadioButton, 'radioButton_Tripolar')
+
+        # Labels
+        # ----------------------------------------------------------------------------------------------
+
+        # Define a label para alterar a imagem da configuração 
+        self.label_ImagemCondutor = self.findChild(QtWidgets.QLabel, 'label_ImagemCondutor')
+
+        # Combo Boxs
+        # ----------------------------------------------------------------------------------------------
+
+        # Define a Combo Box para o Material isolante
+        self.combobox_MaterialIsolante = self.findChild(QtWidgets.QComboBox, 'comboBox_MaterialIsolante')
+
+        # Define a Combo Box para o Tipo de condutor
+        self.combobox_TipoCondutor = self.findChild(QtWidgets.QComboBox, 'comboBox_TipoCondutor')
+
+        # Define a Combo Box para o Arranjo da instalação
+        self.combobox_ArranjoInstalacao = self.findChild(QtWidgets.QComboBox, 'comboBox_ArranjoInstalacao')
+
+        # Define a Combo Box para o Nível de tensão
+        self.combobox_NivelTensao = self.findChild(QtWidgets.QComboBox, 'comboBox_NivelTensao')
+
+        # Define a Combo Box para a seção do condutor
+        self.combobox_Secao = self.findChild(QtWidgets.QComboBox, 'comboBox_Secao')
 
 
-        self.btm_configuracao_Unipolar.clicked.connect(self.imagem_unipolar)
-        self.btm_configuracao_Tripolar.clicked.connect(self.imagem_tripolar)
+        # Line Edit
+        # ----------------------------------------------------------------------------------------------
+
+        # Define o Line Edit para a variável Rp
+        self.lineEdit_RP = self.findChild(QtWidgets.QLineEdit, "lineEdit_RP")
+        
+        # Define o Line Edit para a variável Rz
+        self.lineEdit_RZ = self.findChild(QtWidgets.QLineEdit, "lineEdit_RZ")
+        
+        # Define o Line Edit para a variável Xp
+        self.lineEdit_XP = self.findChild(QtWidgets.QLineEdit, "lineEdit_XP")
+        
+        # Define o Line Edit para a variável Xz
+        self.lineEdit_XZ = self.findChild(QtWidgets.QLineEdit, "lineEdit_XZ")
+        
+        # Define o Line Edit para a variável Xc
+        self.lineEdit_XC = self.findChild(QtWidgets.QLineEdit, "lineEdit_XC")
+
+        # Define o Line Edit para a variável Corrente nominal
+        self.lineEdit_CorrenteNominal = self.findChild(QtWidgets.QLineEdit, "lineEdit_CorrenteNominal")
+        
+        
+        # Define a ação ao clicar no radio button unipolar
+        self.radioButton_Unipolar.clicked.connect(self.imagem_unipolar)
+
+        # Define a ação ao clicar no radio button tripolar
+        self.radioButton_Tripolar.clicked.connect(self.imagem_tripolar)
 
 
 
     @pyqtSlot()
     def imagem_unipolar(self):
         imagem = QPixmap('Imagens/Cabo Unipolar.png')
-        self.imagem_condutor.setPixmap(imagem)
+        self.label_ImagemCondutor.setPixmap(imagem)
 
     @pyqtSlot()
     def imagem_tripolar(self):
         imagem = QPixmap('Imagens/Cabo Tripolar.png')
-        self.imagem_condutor.setPixmap(imagem)
+        self.label_ImagemCondutor.setPixmap(imagem)
 
 
 # Função Main
--- planilhas.py
+++ planilhas.py
@@ -1,33 +1,11 @@
 import pandas as pd
-import platform
 
-sistema = platform.system() 
 
 def Planilha(equipamento, tabela):
-    """Função para criar a tabela de um arquivo do Excel
 
-    Parâmetros:
-
-        equipamento: string
-
-            Equipamento que se precisa consultar a tabela
-
-        tabela: string
-
-            Número da Tabela referente ao livro do Mamede 4ª Ed """
-
-    
     try:
         # Variável que armazena a planilha em formato Pandas    
-        if(sistema == "Windows"):
-            sheet = pd.read_excel(f"Tabelas/{equipamento}.xlsx", sheet_name = tabela)
-        
-        # Fiz isso pois não sei se o diretório no sistema linux funciona da mesma maneira. Caso tenha erro, mudar essa parte do código.
-        elif(sistema == "Linux"):
-            sheet = pd.read_excel(f"Tabelas/{equipamento}.xlsx", sheet_name = tabela)
-
-        else:
-            exit("Sistema Operacional não reconhecido.")
+        sheet = pd.read_excel(f"Tabelas/{equipamento}.xlsx", sheet_name = tabela)
 
     except:
         # Sai do programa caso o nome da planilha esteja errado
@@ -38,7 +16,7 @@ def Planilha(equipamento, tabela):
 
 def main():
 
-    tabela = Planilha("Condutores", "4.21")
+    tabela = Planilha("Tabelas", "4.23.tri")
 
     print(tabela.head())
 
