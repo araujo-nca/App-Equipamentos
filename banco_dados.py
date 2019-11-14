292dcfc844682be70d8897e8db509e55193ce1f8
Nathan Araujo <nathandearaujo13@gmail.com>
2019-11-14 17:59:54 -0300

    conserto banco de dados e adicao da tabela conduto

--- banco_dados.py
+++ banco_dados.py
@@ -7,20 +7,3 @@ tab46 = Planilha("Condutores","4.6")
 tab47 = Planilha("Condutores","4.7")
 tab48 = Planilha("Condutores","4.8")
 tab49 = Planilha("Condutores","4.9")
-
-tab621 = Planilha("Transformador de Potencial", "6.2.1")
-tab622 = Planilha("Transformador de Potencial", "6.2.2")
-tab641 = Planilha("Transformador de Potencial", "6.4.1")
-tab642 = Planilha("Transformador de Potencial", "6.4.2")
-tab66 = Planilha("Transformador de Potencial", "6.6")
-
-tab51 = Planilha("Transformador de Corrente", "5.1")
-tab52 = Planilha("Transformador de Corrente", "5.2")
-tab53 = Planilha("Transformador de Corrente", "5.3")
-tab54 = Planilha("Transformador de Corrente", "5.4")
-tab55 = Planilha("Transformador de Corrente", "5.5")
-tab56 = Planilha("Transformador de Corrente", "5.6")
-tab57 = Planilha("Transformador de Corrente", "5.7")
-
-
-
